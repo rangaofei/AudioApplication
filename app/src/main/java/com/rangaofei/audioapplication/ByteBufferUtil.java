794cf0d9bbe4a23a0e0399556370337d4d21e9aa
saka <876038559@qq.com>
2018-04-16 20:27:03 +0800

    add Test

---
+++ app/src/main/java/com/rangaofei/audioapplication/ByteBufferUtil.java
@@ -0,0 +1,72 @@
+package com.rangaofei.audioapplication;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class ByteBufferUtil {
+    public static short littleByteToShort(byte[] data) {
+        if (data.length != 2) {
+            throw new UnsupportedOperationException("the byte length is not 2");
+        }
+        return ByteBuffer.allocate(data.length).order(ByteOrder.LITTLE_ENDIAN).put(data).getShort(0);
+    }
+
+    public static short bigByteToShort(byte[] data) {
+        if (data.length != 2) {
+            throw new UnsupportedOperationException("the byte length is not 2");
+        }
+        return ByteBuffer.allocate(data.length).order(ByteOrder.BIG_ENDIAN).put(data).getShort(0);
+    }
+
+    public static byte[] shortToLittleByte(short data) {
+        return ByteBuffer.allocate(2).order(ByteOrder.LITTLE_ENDIAN).putShort(data).array();
+    }
+
+    public static byte[] shortToBigByte(short data) {
+        return ByteBuffer.allocate(2).order(ByteOrder.BIG_ENDIAN).putShort(data).array();
+    }
+
+    public static int littleByteToInt(byte[] data) {
+        if (data.length != 4) {
+            throw new UnsupportedOperationException("the byte length is not 4");
+        }
+        return ByteBuffer.allocate(data.length).order(ByteOrder.LITTLE_ENDIAN).put(data).getInt(0);
+    }
+
+    public static int bigByteToInt(byte[] data) {
+        if (data.length != 4) {
+            throw new UnsupportedOperationException("the byte length is not 4");
+        }
+        return ByteBuffer.allocate(data.length).order(ByteOrder.BIG_ENDIAN).put(data).getInt(0);
+    }
+
+    public static byte[] intToLittleByte(int data) {
+        return ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(data).array();
+    }
+
+    public static byte[] intToBigByte(int data) {
+        return ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(data).array();
+    }
+
+    public static long littleByteToLong(byte[] data) {
+        if (data.length != 8) {
+            throw new UnsupportedOperationException("the byte length is not 8");
+        }
+        return ByteBuffer.allocate(data.length).order(ByteOrder.LITTLE_ENDIAN).put(data).getLong(0);
+    }
+
+    public static long bigByteToLong(byte[] data) {
+        if (data.length != 8) {
+            throw new UnsupportedOperationException("the byte length is not 8");
+        }
+        return ByteBuffer.allocate(data.length).order(ByteOrder.BIG_ENDIAN).put(data).getLong(0);
+    }
+
+    public static byte[] longToLittleByte(int data) {
+        return ByteBuffer.allocate(8).order(ByteOrder.LITTLE_ENDIAN).putLong(data).array();
+    }
+
+    public static byte[] longToBigByte(int data) {
+        return ByteBuffer.allocate(8).order(ByteOrder.BIG_ENDIAN).putLong(data).array();
+    }
+}
---
+++ app/src/test/java/com/rangaofei/audioapplication/ByteBufferUtilTest.java
@@ -0,0 +1,91 @@
+package com.rangaofei.audioapplication;
+
+import android.util.Log;
+
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class ByteBufferUtilTest {
+    private byte[] littleShort = new byte[]{0x0a, 0x00};
+    private byte[] bigShort = new byte[]{0x00, 0x0a};
+    private byte[] littleInt = new byte[]{0x0b, 0x00, 0x00, 0x00};
+    private byte[] bigInt = new byte[]{0x00, 0x00, 0x00, 0x0b};
+    private byte[] littleLong = new byte[]{0x0c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    private byte[] bigLong = new byte[]{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0c};
+
+    @Test
+    public void littleByteToShort() {
+        short result = ByteBufferUtil.littleByteToShort(littleShort);
+        assertEquals(result, 10);
+    }
+
+    @Test
+    public void bigByteToShort() {
+        short result = ByteBufferUtil.bigByteToShort(bigShort);
+        assertEquals(result, 10);
+    }
+
+    @Test
+    public void shortToLittleByte() {
+        byte[] result = ByteBufferUtil.shortToLittleByte((short) 10);
+        assertArrayEquals(result, littleShort);
+    }
+
+    @Test
+    public void shortToBigByte() {
+        byte[] result = ByteBufferUtil.shortToBigByte((short) 10);
+        assertArrayEquals(result, bigShort);
+    }
+
+    @Test
+    public void littleByteToInt() {
+        int result = ByteBufferUtil.littleByteToInt(littleInt);
+        assertEquals(result, 11);
+    }
+
+    @Test
+    public void bigByteToInt() {
+        int result = ByteBufferUtil.bigByteToInt(bigInt);
+        assertEquals(result, 11);
+    }
+
+    @Test
+    public void intToLittleByte() {
+        byte[] result = ByteBufferUtil.intToLittleByte(11);
+        assertArrayEquals(result, littleInt);
+    }
+
+    @Test
+    public void intToBigByte() {
+        byte[] result = ByteBufferUtil.intToBigByte(11);
+        assertArrayEquals(result, bigInt);
+    }
+
+    @Test
+    public void littleByteToLong() {
+        long result = ByteBufferUtil.littleByteToLong(littleLong);
+        assertEquals(result, 12);
+    }
+
+    @Test
+    public void bigByteToLong() {
+        long result = ByteBufferUtil.bigByteToLong(bigLong);
+        assertEquals(result, 12);
+    }
+
+    @Test
+    public void longToLittleByte() {
+        byte[] result = ByteBufferUtil.longToLittleByte(12);
+        assertArrayEquals(result,littleLong);
+    }
+
+    @Test
+    public void longToBigByte() {
+        byte[] result = ByteBufferUtil.longToBigByte(12);
+        assertArrayEquals(result,bigLong);
+    }
+}
